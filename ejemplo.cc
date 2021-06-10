4f35c7fff48db3d083fe98e8b6f1369184c5eac8
brandi003 <brandon.diaz@alumnos.uv.cl>
2021-06-10 13:19:10 -0400

    Update ejemplo.cc

--- ejemplo.cc
+++ ejemplo.cc
@@ -1,6 +1,66 @@
 #include <global.hh>
 #include <Timer.hpp>
 
+void get_vecinos(int x, int y, char* matriz, char* vacia){
+	fil=sizeof(matriz)/sizeof(matriz[0]);
+	col=sizeof(matriz[0])/sizeof(matriz[0][0]);
+	int vecinos[]=[[x-1,y-1],[x,y-1],[x+1,y-1],[x-1,y],[x+1,y],[x-1,y+1],[x,y+1],[x+1,y+1]];
+
+	for (size_t i=8;i>=0;i--){
+		if(vecinos[i][0]<0 || vecinos[i][0]>(col-1)){
+			std::erase(vecinos,vecinos[i]);
+			continue;
+		}
+		if(vecinos[i][1]<0 || vecinos[i][1]>(col-1)){
+			std::erase(vecinos,vecinos[i]);
+			continue;
+		}
+	}
+}
+
+void mostrar(char* matriz){
+	for (size_t i = 0 ; i<(sizeof(matriz)/sizeof(matriz[0])) : i++){
+		for (size_t j = 0 ; j < (sizeof(matriz[0])/sizeof(matriz[0][0])) ; j++){
+			std::cout << matriz[i][j] << ",";
+		}
+		std::cout << std:endl;
+	}
+}
+
+char* matriz_vacia(int col, int fil){
+	char[fil][col] vacia;
+	for (size_t i = 0 ; i < fil ; i++){
+		for (size_t j = 0 ; j < col ; j++){
+			vacia[i][j]="#"
+		}
+	}
+	return matriz;
+}
+
+int main(int argc , char *argv []) {
+	std::random_device rd;
+    std::default_random_engine eng(rd());
+    std::uniform_real_distribution<float> distr(0, 1);
+	int col=5;
+	int fil=5;
+	float prob=0.8;
+	char[fil][col] matriz;
+	for (size_t i = 0 ; i < fil ; i++){
+		for (size_t j = 0 ; j < col ; j++){
+			if(prob>=distr(eng)){
+				matriz[i][j]="*";
+			}else{
+				matriz[i][j]="#"
+			}
+		}
+	}
+	mostrar(matriz);
+
+	return(EXIT_SUCCESS);
+}
+
+
+/*
 int main(int argc , char *argv []) {
 	std::random_device dev;  
 	
@@ -47,4 +107,4 @@ int main(int argc , char *argv []) {
 	
 	
 	return(EXIT_SUCCESS);
-}
+}*/
