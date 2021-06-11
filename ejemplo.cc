5a70a0e04df9b4581cd2baf9fc9588a0d336332c
brandi003 <brandon.diaz@alumnos.uv.cl>
2021-06-11 15:42:19 -0400

    Update ejemplo.cc

--- ejemplo.cc
+++ ejemplo.cc
@@ -55,6 +55,7 @@ char** stepP(char **matriz, int fil, int col, int32_t nt){
     char** vacia=generar_vacia(fil,col);
     #pragma omp parallel for num_threads(nt)
     for (int i=0 ;  i<fil ; i++){
+    	#pragma omp parallel for num_threads(nt)
     	for (int j=0 ; j<col ; j++){
     		int cont=0;
     		int vecinos[8][2]={{i-1,j-1},{i,j-1},{i+1,j-1},{i-1,j},{i+1,j},{i-1,j+1},{i,j+1},{i+1,j+1}};
