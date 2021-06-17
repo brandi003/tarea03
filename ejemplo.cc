1335d156563de5ffe8b23a8c9af6dcac6383d269
brandi003 <brandon.diaz@alumnos.uv.cl>
2021-06-16 21:38:19 -0400

    Update ejemplo.cc

--- ejemplo.cc
+++ ejemplo.cc
@@ -422,7 +422,6 @@ int main(int argc , char *argv []){
             
         }
         std::cout << "elapsed:" << time << "ms\n";
-        std::cout << "elapsed everage per iteration:" << time/iter << "ms\n";
     }
     
     
